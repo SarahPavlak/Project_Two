805434b47bbeb9b8bdae27ccf1aa14b1000f668b
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-04-10 18:16:14 -0400

    Create freestyle.py
    allows a user to input bedroom, bathroom, budget amounts

---
+++ freestyle.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from tkinter import *
+import datetime
+
+
+print("To start, please input your apartment preferences")
+
+#TNKTR USERINPUTS----------------------------------------------------------
+#https://github.com/s2t2/shopping-cart-project/blob/master/shopping_cart.py
+#!/usr/bin/python3
+#https://www.python-course.eu/tkinter_entry_widgets.php
+#https://www.python-course.eu/tkinter_text_widget.php
+
+
+def bed():
+    bed_value=[l1.get(i) for i in l1.curselection()]
+    print("You have selected: ")
+    print(bed_value)
+
+def bath():
+    bath_value=[l2.get(i) for i in l2.curselection()]
+    print(bath_value)
+
+def budget():
+    budget_value=[l3.get(i) for i in l3.curselection()]
+    print(budget_value)
+
+w1= Tk()
+w1.title('Apartment Selection App')
+
+l1= Listbox(w1, selectmode= MULTIPLE, width= 20)
+l2= Listbox(w1, selectmode= MULTIPLE, width= 20)
+l3= Entry(width= 20)
+
+
+#Bedroom Selection-------------------------------------------------------
+T = Text(w1, height=2, width=30)
+T.pack()
+T.insert(END, "Please select your desired\n number of bedrooms: ")
+
+bedroom= ['Studio', 'One Bedroom', 'Two Bedrooms','More than Two Bedrooms']
+for val in bedroom:
+    l1.insert(END, val)
+l1.pack()
+
+b1=Button(text= 'Select', command=bed)
+b1.pack()
+
+#Bathroom Selection-------------------------------------------------------
+T = Text(w1, height=2, width=30)
+T.pack()
+T.insert(END, "Please select your desired\n number of bathrooms: ")
+
+bathroom= ['One bathroom', 'Two Bathrooms', 'More than Two Bathrooms']
+for val in bathroom:
+    l2.insert(END, val)
+l2.pack()
+
+b2=Button(text= 'Select', command=bath)
+b2.pack()
+
+#Budget Selection-------------------------------------------------------
+T = Text(w1, height=2, width=30)
+T.pack()
+T.insert(END, "Please input your budget: ")
+
+budget = [0]
+for val in budget:
+    l3.insert(END, val)
+l3.pack()
+
+b3=Button(text='Select', command=budget)
+b3.pack()
+
+#Quit Button-----------------------------------------------------------
+b4 = Button(w1, text='Quit', command=w1.quit)
+b4.pack()
+
+w1.mainloop()
+breakpoint
+
+t = datetime.datetime.now()
+print("User response was:")
+#to insert user response
+
+print("Response recorded at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%m:%S")) 
+
+
