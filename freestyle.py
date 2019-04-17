eedcf3eb3a61a5150fe7c5e139e3d2ba10bf574d
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-04-17 18:35:05 -0400

    Update freestyle.py
    got website scraping

--- freestyle.py
+++ freestyle.py
@@ -237,7 +237,45 @@ breakpoint
 
 
     #Scraping the Website--------------------------------------------------
+# adapted from: https://selenium-python.readthedocs.io/waits.html
 
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+URL = "https://www.avaloncommunities.com/virginia/arlington-apartments/avalon-ballston-square/floor-plans"
+
+driver = webdriver.Chrome("/usr/local/bin/chromedriver") # location where chromedriver is installed
+
+driver.get(URL)
+
+try:
+    #div = driver.find_element_by_id("floor-plan-listing") #> selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"id","selector":"floor-plan-listing"}
+    listings_appear = EC.presence_of_element_located((By.ID, "floor-plan-listing"))
+    wait_duration = 3 # seconds
+    div = WebDriverWait(driver, wait_duration).until(listings_appear)
+    print("PAGE LOADED!")
+except TimeoutException:
+    print("TIME OUT!")
+finally:
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+
+    one_br_layouts = soup.find("div", id="bedrooms-1").findAll("div", "row")
+    for layout in one_br_layouts:
+        #table=(layout.find("table")["h4"])
+        one_br_headers=(layout.find("table").find("thead").find("tr").text)
+        print(one_br_headers)
+        
+        one_br_listings=(layout.find("table").find("tbody").find("tr").text)
+        print(one_br_listings)
+      
+        #breakpoint()
 
 
 
