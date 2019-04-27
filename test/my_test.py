5cd268cb5d4853627f755937f9dba738b036d7dc
SarahPavlak <scp61@georgetown.edu>
2019-04-27 12:07:37 -0400

    Update Test

--- test/my_test.py
+++ test/my_test.py
@@ -2,4 +2,4 @@ from app.my_script import enlarge
 
 def test_enlarge(): # note the function name is prefixed with "test_"
     result = enlarge(3) # directly invoke the function we want to test
-    assert result == 300 # describe expectations for desired behavior
+    assert result == 30000 # describe expectations for desired behavior
