7b82114fa0d419fc997b15d4c388f1bee9315f2a
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-04-28 19:58:20 -0400

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,3 +1,12 @@
-def enlarge(i):
-    return i * 100
+from my_script import enlarge, notification
 
+def test_enlarge(): 
+    result = enlarge 
+    assert result == 'avalon-ballston-square' or 'ava-ballston' or  'ava-ballston,avalon-ballston-square' 
+
+def test_notification(): 
+    result = notification
+    assert result == 'One time' or 'Recurring' or 'One time,Recurring'
+
+
+#testing that apartment and notification csv has a value!
